653a8103e6cc16d48e37a23e01ec9929a09c4da8
Juan David <juandavidq2367@gmail.com>
2024-11-05 09:57:23 -0500

    vercel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'; // Asegúrate de importar useState
-import { BrowserRouter as Router, Route, Routes, Navigate, BrowserRouter } from 'react-router-dom'; // Asegúrate de importar Navigate
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'; // No es necesario importar BrowserRouter aquí
 import Register from './components/pages/Register';
 import Login from './components/pages/Login';
 import Dashboard from './components/pages/Dashboard';
@@ -11,25 +11,24 @@ function App() {
   const [userId, setUserId] = useState(null);
 
   const handleLogin = (userId) => {
-        console.log("Usuario logueado con ID:", userId); // Esto debería mostrar el ID
-        setUserId(userId);
-    };
+    console.log("Usuario logueado con ID:", userId); // Esto debería mostrar el ID
+    setUserId(userId);
+  };
 
   return (
-    <BrowserRouter>
-      <Router>
-        <Routes>
-          <Route index element={<Login callback={handleLogin} />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/LoginAD" element={<LoginAdmin />} />
-          <Route path="/RegistroAD" element={<RegistroAdmin />} />
-          <Route path="/RegistroGan" element={<RegistroGan />} />
-          <Route path='/dashboard' 
-            element={userId ? <Dashboard userId={userId} /> : <Navigate to="/" replace />} 
-          />
-        </Routes>
-      </Router>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route index element={<Login callback={handleLogin} />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/LoginAD" element={<LoginAdmin />} />
+        <Route path="/RegistroAD" element={<RegistroAdmin />} />
+        <Route path="/RegistroGan" element={<RegistroGan />} />
+        <Route path='/dashboard' 
+          element={userId ? <Dashboard userId={userId} /> : <Navigate to="/" replace />} 
+        />
+      </Routes>
+    </Router>
   );
 }
+
 export default App;
